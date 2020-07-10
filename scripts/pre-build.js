e7f80f826f86a5391d743420bf6d5571b4159a44
PhasmaExMachina <cathryne.trallis@gmail.com>
2020-07-10 19:27:32 -0400

    Update readme

--- scripts/pre-build.js
+++ scripts/pre-build.js
@@ -1,5 +1,12 @@
 const fs = require('fs'),
-      path = require('path')
+      path = require('path'),
+      existingVersion = require('../src/version.json'),
+      {execSync} = require('child_process')
 
 const versionName = fs.readFileSync(path.join(__dirname, '../android/app/build.gradle'), 'utf8').match(/versionName "(\d+\.\d+\.\d+)"/)[1]
-fs.writeFileSync(path.join(__dirname, '../src/version.json'), JSON.stringify(versionName))
+if(existingVersion !== versionName) {
+  fs.writeFileSync(path.join(__dirname, '../src/version.json'), JSON.stringify(versionName))
+  execSync(`git add src/version.json && git commit -m "Set app version to v${versionName}"`,
+    {stdio: 'inherit'}
+  )
+}
